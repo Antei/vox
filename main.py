5362e7f541696c267e0b6d0e9f218b67bc772405
Antei Hasgard <antei.hasgard@gmail.com>
2022-06-15 17:55:32 +0300

    Правки main

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import speech_recognition
 import os
 import random
-import pyttsx3
-from datetime import datetime
 import time
+from datetime import datetime
 from threading import Thread
 
+import pyttsx3
+import speech_recognition
 
 # подключаем команды ассистента
-from commands import *
+from commands import assistant_names, commands_dict
 
-
+current_time = datetime.now()
 sr = speech_recognition.Recognizer()
 sr.pause_threshold = 0.5
 tts_engine = pyttsx3.init()
-current_time = datetime.now()
 
 
 def listen_commands():
-
     try:
         with speech_recognition.Microphone() as mic:
             sr.adjust_for_ambient_noise(source=mic, duration=0.5)
@@ -26,29 +24,22 @@ def listen_commands():
             audio = sr.listen(source=mic)
             rec_data = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
         return rec_data
-
     except speech_recognition.UnknownValueError:
-        print('Ошибка распознавания')
-
-    except speech_recognition.WaitTimeoutError:
         pass
-    
+    except speech_recognition.WaitTimeoutError:
+        pass  
     except speech_recognition.RequestError:
         print('Нет сети')
 
 
 def play_speech(text_to_speech):
-
     # Воспроизведение ответов ассистента
-
     tts_engine.say(str(text_to_speech))
     tts_engine.runAndWait()
 
 
 def greetings():
-
     # приветствие, случайное, в зависимости от времени суток
-
     greetings = ['добрый день', 'привет', 'здравствуйте',
                  'приветствую', 'добрый вечер']
     current_hour = current_time.hour
@@ -64,7 +55,6 @@ def greetings():
 
 
 def create_task():
-
     play_speech('что нужно добавить в заметку?')
 
     note = listen_commands()
@@ -72,14 +62,12 @@ def create_task():
     if note:
         with open('todo-list.txt', 'a', encoding='UTF-8') as file:
             file.write(f'- {note}\n')
-
         play_speech(f'Задача "{note}" успешно добавлена в список дел.')
     else:
         play_speech('Нет данных для добавления')
 
 
 def play_music():
-
     if os.path.exists('music'):
         files = os.listdir('music')
         random_file = f'music/{random.choice(files)}'
@@ -90,14 +78,12 @@ def play_music():
 
 
 def what_a_time():
-
     current_hour = current_time.hour
     current_minute = current_time.minute
     play_speech(f'Сейчас {current_hour}:{current_minute}')
 
 
 def timer():
-
     play_speech('на сколько ставить таймер?')
 
     time_to = listen_commands()
@@ -122,15 +108,12 @@ def timer():
                 plur = x    
     
     play_speech(f'таймер установлен на {int(local_time)} {plur}')
-    print(int(local_time), plur)
     time.sleep(local_time * multiplier)
     play_speech(f'таймер на {int(local_time)} {plur} закончился')
 
 
-def play_farewell_and_quit():
-    
+def farewell_and_quit():
     # прощание в зависимости от времени суток и выход из программы
-    
     farewells = ['доброго дня', 'всего доброго', 'пока',
                  'до встречи', 'хорошего вечера']
     current_hour = current_time.hour
@@ -148,25 +131,36 @@ def play_farewell_and_quit():
     quit()
 
 
+commands = {
+    'greetings': greetings,
+    'create_task': create_task,
+    'play_music': play_music,
+    'what_a_time': what_a_time,
+    'farewell_and_quit': farewell_and_quit,
+    'timer': timer,
+}
+
+
 # основная функция
 def main():
-
     while True:
         print('ожидаю...')
         query = listen_commands()
         if query:
-            assistant_name, command = str(query).split(" ", 1)
-        else:
-            continue
-
-        if assistant_name in assistant_names:
-            for key, value in commands_dict.items():
-                if command in value:
-                    if key == timer:
-                        th_timer = Thread(target=timer, args=())
-                        th_timer.start()
-                    else:
-                        key()
+            divided_query = str(query).split(' ', 1)
+            if len(divided_query) > 1:
+                assistant_name, command = divided_query
+                print(assistant_name, command)
+                if assistant_name in assistant_names:
+                    for key, value in commands_dict.items():
+                        if command in value:
+                            if key == timer:
+                                th_timer = Thread(target=timer, args=())
+                                th_timer.start()
+                            else:
+                                commands[key]()
+            else:
+                play_speech('повторите запрос')
         else:
             continue
 
